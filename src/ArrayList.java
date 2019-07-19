904c82a832cd357e7a4067286edf48a6cd960aa6
ganesamurugan <ganesamurugan3@gmail.com>
2019-07-19 12:33:46 +0530

    I am attached array list

---
+++ src/ArrayList.java
@@ -0,0 +1,13 @@
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+
+		String[] st={"sachin","dravid","ganguly","dhoni"};
+		
+		ArrayList al=new ArrayList();
+		
+		
+	}
+
+}
